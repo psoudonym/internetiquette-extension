d3ee7827aab1e2ff04743f0fd2c995e05b9f8b2a
Robert Jenkins <bobert@gmail.com>
2014-08-23 00:39:04 -0400

    - progress with data driven extension

--- replace.js
+++ replace.js
@@ -1,4 +1,9 @@
-
-$('* :not(:has(*))').text(function(i, v) {
-return v.replace(/jQuery/ig, 'boners');
-});
+$.getJSON("http://internetiquette.herokuapp.com/mappings.json", function(data) {
+  $('* :not(:has(*))').text(function(i, value) {
+    $.each( data, function( bad_phrase, good_phrase ) {
+      var regex = new RegExp(bad_phrase,"ig");
+      value = value.replace(regex, good_phrase);
+    });
+    return value;
+  });
+})
