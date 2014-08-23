06f506ef0814a855b3d84a498ad9c668f54a1c99
Robert Jenkins <bobert@gmail.com>
2014-08-23 00:46:25 -0400

    - replace.js now pulls data from http://internetiquette.herokuapp.com/mappings.json

--- replace.js
+++ replace.js
@@ -1,8 +1,13 @@
+//Pull data from internetiquette.herokuapp.com/mappings.json
 $.getJSON("http://internetiquette.herokuapp.com/mappings.json", function(data) {
+  //look for text nodes
   $('* :not(:has(*))').text(function(i, value) {
-    $.each( data, function( bad_phrase, good_phrase ) {
+    //iterate over all key value pairs found in the mappings portion of the json response
+    $.each( data.mappings, function( bad_phrase, good_phrase ) {
+      //create a regex object that will look for all case insentive occurence of the bad phrase
       var regex = new RegExp(bad_phrase,"ig");
-      value = value.replace(regex, good_phrase);
+      //replace the bad phrase with the good phrase and store the value
+      value = value.replace(regex, good_phrase.toUpperCase());
     });
     return value;
   });
